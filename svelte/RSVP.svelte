71425d1f7ca155a416ab270a5f165ffae3eb5351
Kaiting Chen <ktchen14@gmail.com>
2021-07-18 16:00:53 -0400

    Add back Google Maps iframes

--- svelte/RSVP.svelte
+++ svelte/RSVP.svelte
@@ -40,7 +40,7 @@
 
   <p>The ceremony and reception will take place at the <a href="https://computerhistory.org/" target=_blank>Computer History Museum</a> on Saturday, October 23<sup>rd</sup> at 05:30 PM PDT. The dress code is black tie optional. This will be an adults-only (21+) celebration.</p>
 
-  <!--<iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJm7NJkla3j4AR8vR-HWRxgOo&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>-->
+  <iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJm7NJkla3j4AR8vR-HWRxgOo&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>
 
   <Toggle name={reception.name} bind:result={$reception}>
     <p class="question">I will attend the wedding ceremony and reception</p>
@@ -72,7 +72,7 @@
 
     <p>You're welcome to attend a cocktail reception at <a href="https://vinolocale.org/" target=_blank>Vino Locale</a> on Friday, October 22<sup>nd</sup>, the day before the wedding, at 07:00 PM PDT.</p>
 
-    <!--<iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2S5OXzi7j4ARGnV-XOyU-4g&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>-->
+    <iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2S5OXzi7j4ARGnV-XOyU-4g&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>
 
     <Toggle name={cocktail.name} bind:result={$cocktail}>
       <p class="question">I will attend the cocktail reception</p>
@@ -108,7 +108,7 @@
 
       <p>You're invited to brunch at <a href="https://changan-artisan-noodle.business.site/" target=_blank>Chang'an Artisan Noodle</a> on Sunday, October 24<sup>th</sup>, the day after the wedding, at 11:00 AM PDT.</p>
 
-      <!--<iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ6X3Saaiwj4AR-ixm6fKP-rI&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>-->
+      <iframe height="450" style="border: 0; width: 100%;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ6X3Saaiwj4AR-ixm6fKP-rI&key=AIzaSyAUU6DgJ-dorKgFb0XMCZ8qACjCRkcXY70"></iframe>
 
       <Toggle name={brunch.name} bind:result={$brunch}>
         <p class="question">I will attend brunch</p>
